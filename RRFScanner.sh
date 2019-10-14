e3a53ffc2ba47f6ce6c7be97787a5448ecf2ab9d
Armel FAUVEAU <armel.fauveau@globalis-ms.com>
2019-10-14 21:36:15 +0200

    Add sounds

--- RRFScanner.sh
+++ RRFScanner.sh
@@ -9,10 +9,8 @@ if [ -z "$1" ]; then
 	/usr/bin/pgrep -f 'python /opt/RRFScanner/RRFScanner.py'
 	pid=$?
 	if [ $pid != 1 ]; then
-        cp /opt/RRFScanner/sounds/active.wav /opt/RRFScanner/status.wav
 		set -- 'stop'
 	else
-        cp /opt/RRFScanner/sounds/desactive.wav /opt/RRFScanner/status.wav
 		set -- 'start'
 	fi
 fi
@@ -21,9 +19,11 @@ case "$1" in
     start)
         echo "Starting RRFScanner"
         nohup python $PATH_SCRIPT --room RRF --sleep 5  --debug False > $PATH_LOG/RRFScanner.log 2>&1 & echo $! > $PATH_PID/RRFScanner.pid
+        cp /opt/RRFScanner/sounds/desactive.wav /opt/RRFScanner/status.wav
         ;;
     stop) 
         echo "Stopping RRFScanner"
         kill `cat $PATH_PID/RRFScanner.pid`
+        cp /opt/RRFScanner/sounds/active.wav /opt/RRFScanner/status.wav
         ;;
     esac
