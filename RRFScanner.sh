c54cc12409b2a585bd821895e4340083654bfef5
Armel FAUVEAU <armel.fauveau@globalis-ms.com>
2019-10-14 20:58:46 +0200

    Add sounds

--- RRFScanner.sh
+++ RRFScanner.sh
@@ -18,11 +18,13 @@ fi
 case "$1" in
     start)
         echo "Starting RRFScanner"
+        rm /usr/share/svxlink/sounds/fr_FR/PropagationMonitor/name.wav
         ln -s /opt/RRFScanner/sounds/active.wav /usr/share/svxlink/sounds/fr_FR/PropagationMonitor/name.wav
         nohup python $PATH_SCRIPT --room RRF --sleep 5  --debug False > $PATH_LOG/RRFScanner.log 2>&1 & echo $! > $PATH_PID/RRFScanner.pid
         ;;
     stop) 
         echo "Stopping RRFScanner"
+        rm /usr/share/svxlink/sounds/fr_FR/PropagationMonitor/name.wav
         ln -s /opt/RRFScanner/sounds/desactive.wav /usr/share/svxlink/sounds/fr_FR/PropagationMonitor/name.wav
         kill `cat $PATH_PID/RRFScanner.pid`
         ;;
