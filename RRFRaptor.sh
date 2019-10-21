bd9bed67a6c500d97fc93ffa506ee8478750b67b
Armel FAUVEAU <armel.fauveau@globalis-ms.com>
2019-10-21 04:11:54 +0200

    Refactoring sound

--- RRFRaptor.sh
+++ RRFRaptor.sh
@@ -18,7 +18,8 @@ fi
 case "$1" in
     start)
         echo "Starting RRFRaptor"
-        pkill -f 'python ${PATH_SCRIPT}'
+        search='python ${PATH_SCRIPT}'
+        pkill -f search
         nohup python $PATH_SCRIPT --sleep 1  --debug False > $PATH_LOG/RRFRaptor.log 2>&1 & echo $! > $PATH_PID/RRFRaptor.pid
         echo "201#"> /tmp/dtmf_uhf
         echo "201#"> /tmp/dtmf_vhf
