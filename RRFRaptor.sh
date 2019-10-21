740c1e2942442689114fa57998015679dd2ab0e8
Armel FAUVEAU <armel.fauveau@globalis-ms.com>
2019-10-21 04:15:23 +0200

    Refactoring sound

--- RRFRaptor.sh
+++ RRFRaptor.sh
@@ -20,13 +20,14 @@ case "$1" in
         echo "Starting RRFRaptor"
         search="python ${PATH_SCRIPT}"
         pkill -f "${search}"
-        nohup python $PATH_SCRIPT --sleep 1  --debug False > $PATH_LOG/RRFRaptor.log 2>&1 & echo $! > $PATH_PID/RRFRaptor.pid
+        nohup python $PATH_SCRIPT --sleep 1  --debug False > $PATH_LOG/RRFRaptor.log 2>&1
         echo "201#"> /tmp/dtmf_uhf
         echo "201#"> /tmp/dtmf_vhf
         ;;
     stop) 
         echo "Stopping RRFRaptor"
-        kill `cat $PATH_PID/RRFRaptor.pid`
+        search="python ${PATH_SCRIPT}"
+        pkill -f "${search}"
         echo "202#"> /tmp/dtmf_uhf
         echo "202#"> /tmp/dtmf_vhf
         ;;
