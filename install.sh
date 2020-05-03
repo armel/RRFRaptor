f591a9e1f056383a90eb67f1e251c64587d5af42
Armel FAUVEAU <armel.fauveau@globalis-ms.com>
2020-05-03 02:18:58 +0200

    Move to Python3

--- install.sh
+++ install.sh
@@ -7,11 +7,6 @@ echo "+-----------------------+"
 echo "Step 2 - Some pip install"
 echo "+-----------------------+"
 pip3 install requests
-echo "+----------------------+"
-echo "Step 3 - Patch Logic.tcl"
-echo "+----------------------+"
-mv /usr/share/svxlink/events.d/local/Logic.tcl /usr/share/svxlink/events.d/local/Logic.tcl.bak
-cp /opt/RRFRaptor/Logic.tcl /usr/share/svxlink/events.d/local/Logic.tcl
 echo "+------------------+"
-echo "Step 4 - Rock'n roll"
+echo "Step 3 - Rock'n roll"
 echo "+------------------+"
