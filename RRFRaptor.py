501571f83cee9f88e887135752cbbf984de895ca
Armel FAUVEAU <armel.fauveau@globalis-ms.com>
2020-10-24 02:06:10 +0200

    Add parking feature

--- RRFRaptor.py
+++ RRFRaptor.py
@@ -77,6 +77,8 @@ def main(argv):
                 s1 = s.room[s.current_room]['last']
                 s2 = time.time()
 
+                print((s1 - s2), s.sleep)
+
                 if (s2 - s1) > s.sleep: # Si la limite de temporisation atteinte, on scan
                     if s.parking is True and s.current_room != s.default_room:
                         s.current_room = s.default_room
